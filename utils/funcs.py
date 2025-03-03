140420df83340f9dea96a3cdfd574b44062f7d8b
Shaoxing Mo <31978201+njujinchun@users.noreply.github.com>
2025-03-03 21:54:21 +0800

    Add files via upload

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import ImageGrid
@@ -7,64 +8,48 @@ import torch as th
 plt.switch_backend('agg')
 
 
-# plotting
-def plot_loss(save_dir, logger):
-    fig = plt.figure()
-    steps = len(logger['loss_d'])
-    plt.plot(range(1, steps + 1), logger['loss_d'], label='loss_d')
-    plt.plot(range(1, steps + 1), logger['loss_g'], label='loss_g')
-    plt.legend()
-    plt.xlabel('Steps')
-    plt.ylabel('Loss')
-    plt.savefig(save_dir + '/loss.pdf', dpi=300)
-    plt.close(fig)
-
-
 def load_data(args):
     with h5py.File(args.data_dir + "in_output_data_{}_{}_ERA5.hdf5".format(args.res,args.dataset), 'r') as f:
-        x = f['x'][()]  # P, T, rTWSA: N*C*H*W
-        y = f['y'][()]  # GRACE/FO TWSA: N*H*W
+        x = f['x'][()]  # Nx*C*H*W, auxillary data (P, T, rTWSA; C=3)
+        y = f['y'][()]  # Nx*H*W, GRACE/FO TWSA data
         land_mask = f['land_mask'][()]
-    # the region of interest: 60°S-84°N, 180°W-180°E, 144*360
+
+    # lat: -60 to 84, lon: -180 to 180, H: 144, W: 360
     x, y, land_mask = x[:,:,:144], y[:,:144], land_mask[:144]
-
-    # set the NaN elements (i.e., the non-land grids) as 0.
-    x[x!=x], y[y!=y] = 0., 0.
-
-    # N*H*W -> N*1*H*W
+    x[x!=x], y[y!=y] = 0., 0.  # set the NaN elements as 0.
     y = np.expand_dims(y, axis=1)
 
-    # remove the auxiliary data from 2000-01 to 2002-03
+    # remove the auxiliary between 2000.01 and 2002.03
     year0 = 2000
     start_date = datetime(year0, 1, 1)
     end_date = datetime(2002, 4, 1)
     id_month0 = (end_date.year - start_date.year) * 12 + end_date.month - start_date.month
     x = x[id_month0:id_month0 + y.shape[0]]
-
-    # the 183-month GRACE data (2002-04 to 2017-06) is used for training
-    # and the GRACE-FO data (2018-06-) is used for testing
+    # use the first 183 months (i.e., the GRACE period) for training,
+    # the data gaps within GRACE period have been filled by interpolation
     xtrain, ytrain = x[:183], y[:183]
+    # use the GRACE-FO data for testing, the 11-month data gaps between GRACE and GRACE-FO have been filled by nans
     xtest, ytest = x[194:], y[194:]
-
-    # reorganize the data: N0*C*H*W -> N*T*C*H*W
+    # reorganize the data, x: N0*C*H*W -> N*nt*C*H*W, y: N0*1*H*W -> N*ntf*H*W
     xtrain, yhtrain, ytrain = data_org(xtrain, ytrain, args)
     xtest, yhtest, ytest = data_org(xtest, ytest, args)
     print('Shape of xtrain: {},yhtrain:{}, ytrain: {}, xtest: {}, yhtest: {}, ytest: {}'
           .format(xtrain.shape,yhtrain.shape, ytrain.shape,xtest.shape,yhtest.shape,ytest.shape))
-    
-    # calculate the statistics of the data
-    stats = {}
-    stats['ytrain_mean'] = np.mean(ytrain, 0)
-    stats['ytrain_var'] = np.sum((ytrain - stats['ytrain_mean']) ** 2)
-    # print('y_test_var: {}'.format(ytest_var))
-    stats['ytest_mean'] = np.mean(ytest, 0)
-    stats['ytest_var'] = np.sum((ytest - stats['ytest_mean']) ** 2)
-    # stats['ntrain'] = ytrain.shape[0]
 
-    # number of input channels and output channels
+    # calculate the statistics of the data
+    ytrain_mean = np.mean(ytrain, 0)
+    ytrain_var = np.sum((ytrain - ytrain_mean) ** 2)
+    stats = {}
+    stats['ytrain_mean'] = ytrain_mean
+    stats['ytrain_var'] = ytrain_var
+
+    ytest_mean = np.mean(ytest, 0)
+    ytest_var = np.sum((ytest - ytest_mean) ** 2)
+    stats['ytest_mean'] = ytest_mean
+    stats['ytest_var'] = ytest_var
     stats['ic'], stats['oc'] = xtest.shape[1]*xtest.shape[2]+yhtest.shape[1]*yhtest.shape[2], ytest.shape[1]
 
-    # create the data loaders
+    # create data loaders
     data_train = th.utils.data.TensorDataset(th.FloatTensor(xtrain),
                                              th.FloatTensor(yhtrain),
                                              th.FloatTensor(ytrain))
@@ -77,8 +62,6 @@ def load_data(args):
     test_loader = th.utils.data.DataLoader(data_test,
                                            batch_size=args.batch_size,
                                            shuffle=True)
-
-    # number of pixels in the training and testing data
     npixel_train = len(train_loader.dataset) * train_loader.dataset[0][1].numel()
     npixel_test = len(test_loader.dataset) * test_loader.dataset[0][1].numel()
     stats['npixel_train'] = npixel_train
@@ -88,14 +71,6 @@ def load_data(args):
 
 
 def data_org(x, y, args, stride=1):
-    """
-    Organize the data from the original format, N0*C*H*W, to the format that can be used in the model, N*T*C*H*W.
-    :param x: auxiliary data (i.e., P, T, and rTWSA) during the historical and future months
-    :param y: target data (i.e., GRACE/FO TWSA) during the historical and future months
-    :param args: including the number of historical (nt) and future (ntf) months
-    :param stride: stride for the sliding window (default: 1)
-    :return: the organized data
-    """
     n0, c, h, w = x.shape
     input_window = args.nt + args.ntf
     n = (n0 - input_window) // stride + 1
@@ -109,22 +84,16 @@ def data_org(x, y, args, stride=1):
         Y[:, i] = y[start_x:end_x]
     # T*N*C*H*W -> N*T*C*H*W
     X, Y = np.transpose(X, (1, 0, 2, 3, 4)), np.transpose(Y, (1, 0, 2, 3, 4))
-
-    # the inputs include the auxiliary data during both the historical and future months (xnew)
-    # and the target data during the historical months (yhist)
     xnew, yhist = X, Y[:,:args.nt]
-    # the target data during the future months (ynew)
     ynew = Y[:, args.nt:,0]
 
     return xnew, yhist, ynew
 
 
 def plot_pred(data, epoch, idx, land_mask, args, output_dir):
-    # set the non-land pixels as nan
     land_mask = np.expand_dims(land_mask,axis=0)
     land_mask = np.repeat(land_mask, data.shape[0], axis=0)
     data[land_mask!=land_mask] = np.nan
-
     cols, rows = args.ntf, 4
     vmaxs = np.full((cols*rows,1), np.nan)
     vmaxs[:cols*(rows-2)] = 30
@@ -132,7 +101,6 @@ def plot_pred(data, epoch, idx, land_mask, args, output_dir):
     vmaxs[cols * (rows - 1):] = 5
     vmins = -vmaxs
     vmins[cols * (rows - 1):] = 0
-
     fig, axes = plt.subplots(rows, cols, figsize=(cols*4,rows*2))
     plt.subplots_adjust(hspace=0.16, wspace=0.15)
     ylabels = (['Observations','Predictions','Residuals','STDs'])
@@ -170,8 +138,9 @@ def plot_nse(observed, predicted, land_mask, output_dir):
 
     cols, rows = nt, 1
     vmin, vmax = 0, 1
-    fig, axes = plt.subplots(rows, cols, figsize=(rows * 4, cols * 2))
-    fs = 8  # font size
+    fig, axes = plt.subplots(rows, cols, figsize=(cols * 8, rows * 4))
+    plt.subplots_adjust(hspace=0.16, wspace=0.15)
+    fs = 13  # font size
     for j, ax in enumerate(fig.axes):
         ax.set_aspect('equal')
         ax.set_xticks([])
